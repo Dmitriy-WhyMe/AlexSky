7b65b9eb8571c3a7ad55821705c7b1c153d1b506
Dmitriy <dima.ivahnoff@mail.ru>
2022-02-03 14:05:57 +0300

    DesktopFinish

--- app/js/common.js
+++ app/js/common.js
@@ -30,7 +30,40 @@
 			  nextEl: '.swiper-button-next',
 			  prevEl: '.swiper-button-prev',
 			},
-		  });
+		});
+
+
+		$("#page-up").on("click","a", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+				top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1500);
+		});
+
+		$('#call').magnificPopup({
+			type: 'inline'
+		});
+		$('#apply').magnificPopup({
+			type: 'inline'
+		});
+		$('#thanks').magnificPopup({
+			type: 'inline'
+		});
+		$('#text-popup-one').magnificPopup({
+			type: 'inline'
+		});
+		$('#text-popup-two').magnificPopup({
+			type: 'inline'
+		});
+		$('#text-popup-three').magnificPopup({
+			type: 'inline'
+		});
+		$('#text-popup-four').magnificPopup({
+			type: 'inline'
+		});
+		$('#text-popup-five').magnificPopup({
+			type: 'inline'
+		});
 		
 	});
 })(jQuery);
